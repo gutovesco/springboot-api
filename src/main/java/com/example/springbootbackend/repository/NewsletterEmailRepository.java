2d82e94cb0e4786d90cc0903fceefc2737310719
gutovesco <guto.vesco@hotmail.com>
2020-11-18 12:11:49 -0300

    newsletter email

--- src/main/java/com/example/springbootbackend/repository/NewsletterEmailRepository.java
+++ src/main/java/com/example/springbootbackend/repository/NewsletterEmailRepository.java
@@ -2,6 +2,8 @@ package com.example.springbootbackend.repository;
 
 import com.example.springbootbackend.model.NewsletterEmail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NewsletterEmailRepository extends JpaRepository <NewsletterEmail, Long> {
 }
